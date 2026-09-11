b00536c8a22253075b4c69a9fc30a2a68477342c
StephenKerry <20106900@setu.ie>
2026-09-11 09:43:16 +0100

    Completed Tasky Lab 1 and added a bit else

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
     <div className="container">
 
       <h1>Tasky</h1>
-      <Task title="Laundry" deadline="Tomorrow" description= "urgent">
+      <Task title="Laundry" deadline="Tomorrow" description= "urgent please do it immediately">
         Fold laundry and put away
     </Task>
-      <Task title="Dishes" deadline="Tomorrow" description= " not urgent" />
-      <Task title="Tidy" deadline="Today" description= "urgent" />
+      <Task title="Dishes" deadline="Tomorrow" description= " not urgent leave it for another time" />
+      <Task title="Tidy" deadline="Today" description= " do it in next 40 mins" />
     </div>
   );
 }
